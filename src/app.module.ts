d9d821746de64e5ac7b653222e1eb52ce4b0c350
dinnertime <dinnertime.dev@gmail.com>
2023-12-01 18:06:13 +0900

    event emitter 사용설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 import { UserSubscriptionModule } from './user-subscription/user-subscription.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { eventEmitterConfig } from 'configs/event-emitter.config';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(eventEmitterConfig),
     PrismaModule,
     CommentModule,
     PostModule,
